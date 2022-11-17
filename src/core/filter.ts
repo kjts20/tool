e92c30088ddf6267c76df5a2592c384785517521
wkj <wkj@kjwoo.cn>
2022-11-17 12:05:01 +0800

    添加分页过滤

--- src/core/filter.ts
+++ src/core/filter.ts
@@ -1,10 +1,39 @@
-import { isUndefined } from '../type';
-import { HttpResponse } from './http-server';
+import { isArr, isUndefined } from '../type';
+import { HttpResponse,IPageDataType } from './http-server';
 import { TErrFunc } from './type';
+
+// 分页过滤数据
+export type TPageDataFilter<Item = any> = IPageDataType<Item> & {
+    hasBeenLoad: boolean,
+    isHasMore: boolean,
+    errMsg?:string
+};
 
 // 请求过滤参数
 export interface IHttpServerFilter {
     error: TErrFunc;
+}
+
+// 请求分页数据->过滤分页数据
+export const pageConvert = function<ItemType = any>(pageData: IPageDataType<ItemType>):TPageDataFilter<ItemType>{
+    const _pageData = pageData || {
+        data: [],
+        pageSize: 10,
+        total: 0,
+        pageNum: 0,
+        current: 0,
+        hasBeenLoad: true,
+        isHasMore: false,
+        errMsg: '分页数据获取错误'
+    };
+    const {data, total,pageNum, current} = _pageData;
+    const gData:TPageDataFilter<ItemType> = {
+        ..._pageData,
+        hasBeenLoad: true,
+        isHasMore: pageNum > current && total > 0,
+        errMsg: isArr(data)?null:'数据格式错误'
+    };
+    return gData;
 }
 
 // 请求过滤
@@ -28,6 +57,27 @@ export class ResponseFilter {
                     } else if (isUndefined(res.success, res.code, res.errcode)) {
                         // 如果已经过滤掉请求信息
                         resolve(filterHanlder ? filterHanlder(res) : res);
+                    } else {
+                        reject(res);
+                    }
+                })
+                .catch(reject);
+        });
+    }
+
+    // 对响应数据进行过滤
+    pageFilter<ItemType = any>(responsePromise: Promise<HttpResponse<IPageDataType<ItemType>>>, filterHanlder?: (data:IPageDataType<ItemType>) => any): Promise<any> {
+        return new Promise((resolve: (res: TPageDataFilter<ItemType>) => void, reject) => {
+            responsePromise
+                .then(res => {
+                    const doResolve = pageData => {
+                        resolve(pageConvert(filterHanlder ? filterHanlder(res.data) : res.data));
+                    };
+                    if (res.success) {
+                        doResolve(res.data);
+                    } else if (isUndefined(res.success, res.code, res.errcode)) {
+                        // 如果已经过滤掉请求信息
+                        doResolve(res);
                     } else {
                         reject(res);
                     }
