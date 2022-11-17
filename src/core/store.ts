cee31f7e5c55cd11659ee3a994b7b68ef41b466f
wkj <wkj@kjwoo.cn>
2022-11-17 17:02:30 +0800

    添加仓库

---
+++ src/core/store.ts
@@ -0,0 +1,40 @@
+// 生成保存的key
+const toKey = function(key){
+    return key + '';
+};
+
+// 简易仓库
+export default class Store{
+    store; 
+    constructor(){
+        this.store = new Map();
+    }
+    // 初始化数据
+    init(dataDict:object){
+        for (const key in dataDict) {
+            this.set(key, dataDict[key]);
+        }
+    }
+    // 设置单个数据
+    set(key, value){
+        this.store.set(toKey(key), JSON.stringify(value));
+    }
+    // 获取单个值
+    get(key){
+        const val = this.store.get(toKey(key));
+        if(val !== undefined){
+            return JSON.stringify(val);
+        }else{
+            return undefined;
+        }
+    }
+    
+    // 获取仓库中所有数据
+    getAll(){
+        const dict = {};
+        for (const key of this.store.keys) {
+            dict[key] = this.get(key);
+        }
+        return dict;
+    }
+}
