03ffad5ad06e7c8d24d051dee21ff99bdb47eca3
wkj <wkj@kjwoo.cn>
2022-11-27 22:02:52 +0800

    添加类型

--- src/core/store.ts
+++ src/core/store.ts
@@ -1,34 +1,34 @@
 // 生成保存的key
-const toKey = function(key){
+const toKey = function (key) {
     return key + '';
 };
 
 // 简易仓库
-export class Store{
-    store; 
-    constructor(){
+export class Store {
+    store: Map<string, any>;
+    constructor() {
         this.store = new Map();
     }
 
     // 设置单个数据
-    set(key, value){
+    set(key, value) {
         this.store.set(toKey(key), value);
     }
 
     // 初始化数据
-    sets(dataDict:object){
+    sets(dataDict: object) {
         for (const key in dataDict) {
             this.set(key, dataDict[key]);
         }
     }
 
     // 获取单个值
-    get(key){
+    get(key) {
         return this.store.get(toKey(key));
     }
-    
+
     // 获取仓库中所有数据
-    gets(){
+    gets() {
         const dict = {};
         for (const key of this.store.keys()) {
             dict[key] = this.get(key);
