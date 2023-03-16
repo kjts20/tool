33d84100116041fac420cce469498851b22b0920
wkj <wjkj@kjwoo.cn>
2023-03-16 16:58:15 +0800

    在toFunction中添加上下文

--- src/function.ts
+++ src/function.ts
@@ -1,3 +1,4 @@
+import { isObj } from './type';
 /*
  * @Description: 转换为函数
  * @Author: wkj
@@ -5,15 +6,23 @@
  * @LastEditTime: 2020-09-24 17:53:13
  * @LastEditors: wkj
  */
-export const toFunction = val => {
+export const toFunction = function (val, context = {}) {
     if (typeof val === 'string' && val !== '') {
         try {
-            return new Function('return ' + val)();
+            // 使用的上下文
+            const useContext = isObj(context) ? context : {};
+            // 构造函数
+            const funcName = '__FUNCTION__';
+            const functionContent = `
+                const {${Object.keys(useContext).join(',')}} = this;
+                const  ${funcName} = ${val};
+            `;
+            return new Function(`${functionContent};return ${funcName}`).call(useContext);
         } catch (err) {
             console.error('无法把字符串转换为函数', err);
-            return undefined;
+            return null;
         }
     } else {
-        return undefined;
+        return null;
     }
 };
