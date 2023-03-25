57b8269885b4c9a224eafaa22a96e2e6b248c8df
wkj <wkj@kjwoo.cn>
2023-03-25 12:31:35 +0800

    去掉toFunction的错误捕捉

--- src/function.ts
+++ src/function.ts
@@ -3,25 +3,20 @@ import { isObj } from './type';
  * @Description: 转换为函数
  * @Author: wkj
  * @Date: 2020-09-24 17:50:43
- * @LastEditTime: 2020-09-24 17:53:13
- * @LastEditors: wkj
+ * @LastEditTime: 2023-03-25 12:30:41
+ * @LastEditors: wkj wkj@kjwoo.cn
  */
 export const toFunction = function (val, context = {}) {
     if (typeof val === 'string' && val !== '') {
-        try {
-            // 使用的上下文
-            const useContext = isObj(context) ? context : {};
-            // 构造函数
-            const funcName = '__FUNCTION__';
-            const functionContent = `
-                const {${Object.keys(useContext).join(',')}} = this;
-                const  ${funcName} = ${val};
-            `;
-            return new Function(`${functionContent};return ${funcName}`).call(useContext);
-        } catch (err) {
-            console.error('无法把字符串转换为函数', err);
-            return null;
-        }
+        // 使用的上下文
+        const useContext = isObj(context) ? context : {};
+        // 构造函数
+        const funcName = '__FUNCTION__';
+        const functionContent = `
+             const {${Object.keys(useContext).join(',')}} = this;
+             const  ${funcName} = ${val};
+         `;
+        return new Function(`${functionContent};return ${funcName}`).call(useContext);
     } else {
         return null;
     }
